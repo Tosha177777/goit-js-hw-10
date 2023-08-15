1e0ea4c990983850cbf3dd37a364035d471006d2
Tosha177777 <slavkoanton4@gmail.com>
2023-08-15 18:15:26 +0300

    try ///

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-import Notiflix from 'notiflix';
-const error = document.querySelector('.error');
-error.classList.add('hidden');
 const API_KEY =
   'live_4Tn9geSwD6nYnjlHS5lSpHs1EfvddAhOvLTsr3QhUqbTm79Q07AiBwjtDRRcIikD';
 
@@ -12,25 +9,19 @@ const BREEDS_URL = 'https://api.thecatapi.com/v1/breeds';
 const SEARCH_BREED = 'https://api.thecatapi.com/v1/images/search?breed_ids=';
 
 export function fetchBreeds() {
-  return axios
-    .get(BREEDS_URL)
-    .then(({ data }) => {
-      //   console.log(data);
-      return data;
-    })
-    .catch(err => console.error(err.message));
+  return axios.get(BREEDS_URL).then(({ data }) => {
+    if (!data.length) {
+      throw new Error('No data available');
+    }
+    return data;
+  });
 }
 
 export function fetchCatByBreed(breedId) {
-  return fetch(`${SEARCH_BREED}${breedId}&api_key=${API_KEY}`)
-    .then(res => {
-      if (!res.ok) {
-        error.classList.remove('hidden');
-        throw new Error(res.status);
-      }
-      return res.json();
-    })
-    .catch(err => {
-      Notiflix.Notify.failure(err.message);
-    });
+  return fetch(`${SEARCH_BREED}${breedId}&api_key=${API_KEY}`).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
 }
--- src/index.js
+++ src/index.js
@@ -2,43 +2,58 @@ import { fetchBreeds } from './cat-api.js';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
 
 const selectBreed = document.querySelector('.breed-select');
 const catInfoContainer = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
+errorEl.classList.add('hidden');
 
-// loader.textContent = '';
 loader.classList.add('loading');
 selectBreed.classList.add('hidden');
 catInfoContainer.classList.add('hidden');
 selectBreed.addEventListener('change', onSelect);
 
-fetchBreeds()
-  .then(breeds => {
-    const options = breeds.map(breed => {
-      return { value: breed.id, text: breed.name };
-    });
-
-    new SlimSelect({
-      select: selectBreed,
-      data: options,
-    });
-    loader.classList.remove('loading');
-    selectBreed.classList.remove('hidden');
-  })
-  .catch(error => {
-    console.error(error);
+fetchBreeds().then(breeds => {
+  const options = breeds.map(breed => {
+    return { value: breed.id, text: breed.name };
   });
+
+  new SlimSelect({
+    select: selectBreed,
+    data: options,
+  });
+  loader.classList.remove('loading');
+  selectBreed.classList.remove('hidden');
+});
 
 function onSelect(e) {
   const breedId = e.target.value;
-  console.log(breedId);
-  fetchCatByBreed(breedId).then(resp => {
-    console.log(resp);
-    const markup = resp
-      .map(breeds => {
-        const breed = breeds.breeds[0];
-        return `<div class="cat-image">
+  fetchCatByBreed(breedId)
+    .then(resp => {
+      const markup = resp
+        .map(breeds => {
+          const breed = breeds.breeds[0];
+          return breedMarkup({ breed, breedId, breeds });
+        })
+        .join('');
+      catInfoContainer.classList.remove('hidden');
+      addMarkup(markup, catInfoContainer);
+    })
+    .catch(error => {
+      errorEl.classList.remove('hidden');
+      loader.classList.remove('loading');
+      Notiflix.Notify.failure(error.message);
+    });
+}
+
+function addMarkup(markup, el) {
+  el.innerHTML = markup;
+}
+
+function breedMarkup({ breed, breedId, breeds }) {
+  return `<div class="cat-image">
     <img src="${breeds.url}" alt="${breedId}" id="cat-image" width=360 style="margin-top:20px"/>
   </div>
   <div class="cat-details">
@@ -46,13 +61,4 @@ function onSelect(e) {
     <p id="breed-description">${breed.description}</p>
     <p id="breed-temperament">${breed.temperament}</p>
   </div>`;
-      })
-      .join('');
-    catInfoContainer.classList.remove('hidden');
-    addMarkup(markup, catInfoContainer);
-  });
-}
-
-function addMarkup(markup, el) {
-  el.innerHTML = markup;
 }
